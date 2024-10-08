ddfce5bd93243c9568e54780df158df79f228f15
Mats Kruskopf Eriksson <143085004+Mattan41@users.noreply.github.com>
2024-10-08 15:04:10 +0200

    update movieProfileView. Add formattedDuration to display hours and minutes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,6 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
