962f402fa64ac072fdd03febb989e351fce64ecc
Mats Kruskopf Eriksson <143085004+Mattan41@users.noreply.github.com>
2024-10-09 13:55:03 +0200

    change first letter to uppercaseletter in movieProfileView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '@/views/LandingView.vue'
-import MovieProfileView from '@/views/movieProfileView.vue'
+import MovieProfileView from '@/views/MovieProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
