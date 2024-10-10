df0949d57f394a1385760c88a63fd2da8c6c937d
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-10-10 09:02:43 +0200

    Remove movie link from navigation bar in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,6 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/movie">Movie View</RouterLink>
       </nav>
     </div>
   </header>
