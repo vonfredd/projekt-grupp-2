789ec6b4d0eda8e2c7834ef6722433bab4a91aa5
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-10-15 11:05:29 +0200

    remove unused import statement

--- backend/src/main/java/org/http/backend/repository/MovieRepository.java
+++ backend/src/main/java/org/http/backend/repository/MovieRepository.java
@@ -2,7 +2,6 @@ package org.http.backend.repository;
 
 import org.http.backend.entity.Movie;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
