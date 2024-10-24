74e3b05ffe65dc2060304270088c6a9d5894636b
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-10-24 14:07:05 +0200

    Refactor fetching movie from TMDB in frontend to backend.
    This moves the fetch request to TMDB to be handled in the backend

--- backend/src/main/java/org/http/backend/controller/MovieController.java
+++ backend/src/main/java/org/http/backend/controller/MovieController.java
@@ -1,10 +1,17 @@
 package org.http.backend.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.http.backend.dto.MovieDto;
 import org.http.backend.entity.Movie;
 import org.http.backend.service.MovieService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -12,6 +19,9 @@ import java.util.List;
 @RequestMapping("/movies")
 public class MovieController {
     private final MovieService movieService;
+
+    @Value("${tmdb.access.token}")
+    private String tmdbAccessToken;
 
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
@@ -45,6 +55,30 @@ public class MovieController {
     public ResponseEntity<Void> deleteMovie(@PathVariable String id) {
         movieService.delete(id);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/fetch/{id}")
+    public ResponseEntity<MovieDto> fetchMovieFromTmdb(@PathVariable String id) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.themoviedb.org/3/movie/" + id + "?language=en-US";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("accept", "application/json");
+        headers.set("Authorization", "Bearer " + tmdbAccessToken);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+            MovieDto movieDto = objectMapper.readValue(response.getBody(), MovieDto.class);
+            return ResponseEntity.ok().body(movieDto);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(500).build();
+        }
     }
 
 }
--- backend/src/main/java/org/http/backend/dto/MovieDto.java
+++ backend/src/main/java/org/http/backend/dto/MovieDto.java
@@ -1,5 +1,6 @@
 package org.http.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.http.backend.entity.Movie;
 import org.http.backend.util.Rating;
@@ -7,6 +8,7 @@ import org.http.backend.util.Rating;
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record MovieDto(String id, String title, String overview, String runtime, @JsonProperty("release_date") String releaseDate, @JsonProperty("poster_path") String posterPath, List<GenreDto> genres) {
     public MovieDto {
         if (genres == null) {
