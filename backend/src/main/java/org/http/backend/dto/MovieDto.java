5d5569ec0cc0f2a84ad513c4b48a029558d61b5e
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-10-25 11:15:29 +0200

    add @JsonProperty("backdrop_path") to MovieDto
    - Annotate the backdropPath field in MovieDto with @JsonProperty("backdrop_path") to map the JSON property correctly.

--- backend/src/main/java/org/http/backend/dto/MovieDto.java
+++ backend/src/main/java/org/http/backend/dto/MovieDto.java
@@ -8,7 +8,7 @@ import org.http.backend.util.Rating;
 import java.util.ArrayList;
 import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record MovieDto(String id, String title, String overview, String runtime, @JsonProperty("release_date") String releaseDate, @JsonProperty("poster_path") String posterPath, List<GenreDto> genres, String backdropPath) {
+public record MovieDto(String id, String title, String overview, String runtime, @JsonProperty("release_date") String releaseDate, @JsonProperty("poster_path") String posterPath, List<GenreDto> genres, @JsonProperty("backdrop_path") String backdropPath) {
     public MovieDto {
         if (genres == null) {
             genres = List.of();
