cd8b94092338458aa56735af06785698ff5caae1
Emmelie83 <ej224sk@student.lnu.se>
2024-10-29 11:14:11 +0100

    Change test for landing view according to Mats' suggestion

--- frontend/src/components/Schedules.vue
+++ frontend/src/components/Schedules.vue
@@ -108,7 +108,7 @@ const sortedArr = computed(() => {
           <div v-for="(cinemaSchedules, cinemaName) in sortedArr" :key="cinemaName" class="bg-gray-700 bg-opacity-60 px-5 py-3 mb-4">
             <div class="px-3">
               <h4 class="text-2xl md:text-3xl lg:text-4xl uppercase pb-3 md:pb-4 lg:pb-5 xl:pb-6">{{ cinemaName }}</h4>
-              <div class="flex flex-wrap justify-between gap-3">
+              <div class="flex flex-wrap justify-between gap-5">
                 <div @click="displaySeat(schedule)" v-for="(schedule, index) in cinemaSchedules" :key="index"
                      :class="['rounded-3xl','flex','p-4','uppercase','flex','flex-col','items-center','gap-1',(schedule.cinemaHall.nrOfSeats - bookedSeatsMap.get(schedule.id)) === 0 ? 'bg-secondary' : 'bg-black']">
                   <p class="text-2xl md:text-3xl lg:text-4xl font-semibold">{{ formatDate(schedule.localDateTime) }}</p>
