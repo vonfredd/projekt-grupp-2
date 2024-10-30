dac4af75d8dfa41c7e65ff91ecf6238654067023
Emmelie83 <ej224sk@student.lnu.se>
2024-10-30 16:04:38 +0100

    Add userProfileView with routing, background picture and shaded square where the content will be

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import LandingView from "@/views/LandingView.vue";
 import MovieProfileView from "@/views/MovieProfileView.vue";
 import AdminView from "@/views/AdminView.vue";
+import ProfileView from "@/views/ProfileView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -46,6 +47,11 @@ const router = createRouter({
                 localStorage.removeItem('userData');
                 window.location.href = 'http://localhost:9000/logout';
             },
+        },
+        {
+            path: "/user",
+            name: "userProfile",
+            component: ProfileView,
         },
         {
             path: '/redirect',
---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import Accordion from "@/components/Accordion.vue";
+</script>
+
+<template>
+  <main class="bg-cover min-h-screen bg-fixed relative bg-[bottom_right] bg-[url('/img/userbg.jpg')]">
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div>
+      <div class="bg-gray-700 bg-opacity-90 p-10">
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
