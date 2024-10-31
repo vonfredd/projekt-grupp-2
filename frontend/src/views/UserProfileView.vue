11e018abfac42914cc0a5c1fb04105489086b936
Emmelie83 <ej224sk@student.lnu.se>
2024-10-31 10:38:56 +0100

    Change background picture and opacity

--- frontend/src/views/UserProfileView.vue
+++ frontend/src/views/UserProfileView.vue
@@ -2,10 +2,9 @@
 </script>
 
 <template>
-  <main class="bg-cover min-h-screen bg-fixed relative bg-[bottom_right] bg-[url('/img/userbg.jpg')] flex justify-center">
+  <main class="bg-cover min-h-screen bg-fixed relative bg-left-top bg-[url('/img/userprofilebg.jpg')] flex justify-center">
     <div class="absolute inset-0 bg-black opacity-50"></div>
-    <div class="bg-gray-700 bg-opacity-90 rounded-3xl w-full xl:w-2/3 mx-5 md:mx-10 lg:mx-20 py-10 my-20 relative z-10">
-
+    <div class="bg-gray-700 bg-opacity-50 rounded-3xl w-full xl:w-2/3 mx-5 md:mx-10 lg:mx-20 py-10 my-20 relative z-10">
     </div>
   </main>
 </template>
