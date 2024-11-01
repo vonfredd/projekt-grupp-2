385ce94dd7d5a500f2d8eae34d2f038d4bfbc13c
Emmelie83 <ej224sk@student.lnu.se>
2024-11-01 11:37:27 +0100

    Add font awsome icons

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -54,20 +54,7 @@ onMounted(() => {
             </span></RouterLink>
           <RouterLink v-if="isLoggedIn" to="/user">
           <div class="md:mx-4 lg:mx-6">
-            <svg
-                fill="#000000"
-                width="38px" height="38px"
-                viewBox="0 0 34 34"
-                preserveAspectRatio="xMidYMid meet"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>My account</title>
-              <path
-                  d="M30.61,24.52a17.16,17.16,0,0,0-25.22,0,1.51,1.51,0,0,0-.39,1v6A1.5,1.5,0,0,0,6.5,33h23A1.5,1.5,0,0,0,31,31.5v-6A1.51,1.51,0,0,0,30.61,24.52Z"
-                  class="clr-i-solid clr-i-solid-path-1"></path>
-              <circle cx="18" cy="10" r="7" class="clr-i-solid clr-i-solid-path-2"></circle>
-              <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
-            </svg>
+            <i class="fa-solid fa-user text-black hover:text-gray-700 text-4xl mr-4 mt-1"></i>
           </div>
           </RouterLink>
           <RouterLink v-if="isLoggedIn" to="/logout">
@@ -92,25 +79,19 @@ onMounted(() => {
       <div v-if="isMenuOpen" class="md:hidden">
         <RouterLink v-if="isLoggedIn" to="/user">
           <div class="flex items-center justify-center bg-secondary px-5 rounded-full m-2">
-            <svg fill="#000000" width="20px" height="20px" viewBox="0 0 34 34" preserveAspectRatio="xMidYMid meet"
-              xmlns="http://www.w3.org/2000/svg">
-              <title>My account</title>
-              <path
-                d="M30.61,24.52a17.16,17.16,0,0,0-25.22,0,1.51,1.51,0,0,0-.39,1v6A1.5,1.5,0,0,0,6.5,33h23A1.5,1.5,0,0,0,31,31.5v-6A1.51,1.51,0,0,0,30.61,24.52Z"
-                class="clr-i-solid clr-i-solid-path-1"></path>
-              <circle cx="18" cy="10" r="7" class="clr-i-solid clr-i-solid-path-2"></circle>
-              <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
-            </svg>
+            <i class="fa-solid fa-user"></i>
             <h5 class="block p-3 hover:text-gray-300 uppercase">My account</h5>
           </div>
         </RouterLink>
         <RouterLink v-if="isLoggedIn" to="/logout">
           <div class="flex items-center justify-center bg-secondary px-5 rounded-full mx-2">
+            <i class="fa-solid fa-right-to-bracket"></i>
             <h5 class="block p-3 hover:text-gray-300 uppercase">Logout</h5>
           </div>
         </RouterLink>
         <RouterLink v-if="!isLoggedIn" to="/login">
           <div class="flex items-center justify-center bg-secondary rounded-full px-5 mx-2 mt-2">
+            <i class="fa-solid fa-right-to-bracket"></i>
             <h5 class="block p-3 hover:text-gray-300 uppercase">Login</h5>
           </div>
         </RouterLink>
